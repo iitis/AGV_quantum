fe317e3bc4f2f74a8999e2d0376735f4dd025257
tomsmierz <smierzchalski.tomek@gmail.com>
2022-09-20 01:04:02 +0200

    improved code clarity

--- test/example_0.py
+++ test/example_0.py
@@ -1,6 +1,13 @@
 from src import utils
 from src.linear_solver import solve
 
+"""
+    s0                           
+1 ->    ->
+0 ->    ->
+"""
+
+
 M = 50
 tracks = [("s0",)]
 agv_routes = {0: ("s0",), 1: ("s0",)}
@@ -15,7 +22,7 @@ tau_operation = {(agv, station): 1 for agv in J for station in stations}
 
 
 res, x_iter = solve(M, tracks, agv_routes, d_max, tau_pass, tau_headway, tau_operation,
-                    weights={0: 1, 1: 1/2}, initial_conditions={})
+                    weights={0: 1, 1: 1/2})
 
 if res.success:
     print(utils.see_variables(res.x, x_iter))
