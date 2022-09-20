4e7914d97ff872f4b6392388921af9a85acf15fb
tomsmierz <smirzchalski.tomek@gmail.com>
2022-09-20 18:36:22 +0200

    initial commit. right now the problem is infeasible

---
+++ examples/example_1.py
@@ -0,0 +1,53 @@
+from src import utils
+from src.linear_solver import solve
+
+M = 50
+tracks = [("s0", "s1"), ("s1", "s0"),
+          ("s1", "s2"), ("s2", "s1"),
+          ("s2", "s3"), ("s3", "s2"),
+          ("s3", "s4"), ("s4", "s3"),
+          ("s4", "s5"), ("s5", "s4"),
+          ("s5", "s6")]
+
+# to include track_len into track would require some refactoring
+tracks_len = {("s0", "s1"): 6, ("s1", "s0"): 6,
+              ("s1", "s2"): 6, ("s2", "s1"): 6,
+              ("s2", "s3"): 0, ("s3", "s2"): 0,
+              ("s3", "s4"): 5, ("s4", "s3"): 5,
+              ("s4", "s5"): 4, ("s5", "s4"): 4,
+              ("s5", "s6"): 4, ("s6", "s5"): 4}
+
+agv_routes = {0: ("s0", "s1", "s2", "s3"),
+              1: ("s0", "s1", "s2"),
+              2: ("s4", "s3", "s2", "s1"),
+              3: ("s4", "s3", "s2", "s1", "s0"),
+              4: ("s2", "s3"),
+              5: ("s6", "s5", "s4", "s3"),
+              6: ("s5", "s6")}
+
+stations = utils.create_stations_list(tracks)
+J = utils.create_agv_list(agv_routes)
+agv_routes_as_edges = utils.agv_routes_as_edges(agv_routes)
+all_same_way = utils.create_same_way_dict(agv_routes)
+
+graph = utils.create_graph(tracks, agv_routes)
+
+d_max = {i: 10 for i in J}
+tau_pass = {(j, s, sp): tracks_len[(s, sp)] for j in J for s, sp in agv_routes_as_edges[j]}
+tau_headway = {(j, jp, s, sp): 2 if (s, sp) != ("s2", "s3") and (s, sp) != ("s3", "s2") else 0
+               for (j, jp) in all_same_way.keys() for (s, sp) in all_same_way[(j, jp)]}
+
+tau_operation = {(agv, station): 2 for agv in J for station in stations}
+
+initial_conditions = {("in", 0, "s0"): 0, ("in", 1, "s0"): 0, ("in", 2, "s4"): 8, ("in", 3, "s4"): 9,
+                      ("in", 4, "s2"): 15, ("in", 5, "s6"): 0, ("in", 6, "s5"): 0}
+
+weights = {j: 1 for j in J}
+
+res, x_iter = solve(M, tracks, agv_routes, d_max, tau_pass, tau_headway, tau_operation,
+                    weights, initial_conditions)
+
+if res.success:
+    print(utils.see_variables(res.x, x_iter))
+else:
+    print(res.message)
