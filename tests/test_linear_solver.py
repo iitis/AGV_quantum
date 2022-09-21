79fe91de6edfb87d555db66e1840db2820beddbb
tomsmierz <smierzchalski.tomek@gmail.com>
2022-09-21 23:05:44 +0200

    added tests for tau_headway and headway matrix

--- tests/test_linear_solver.py
+++ tests/test_linear_solver.py
@@ -22,7 +22,8 @@ class SingleStation(unittest.TestCase):
 
     def test_create_bounds_single(self):
         bounds = linear_solver.create_bounds(self.initial_conditions, self.d_max, self.iterators)
-        self.assertEqual(bounds, [(1, 11), (2, 12), (0, 10), (0, 10), (0, 1), (0, 1)])
+        self.assertEqual(bounds, [(1, self.d_max[0] + 1), (2, self.d_max[0] + 2), (0, self.d_max[0]),
+                                  (0, self.d_max[0]), (0, 1), (0, 1)])
 
 
 class MultipleStationsNoOpposite(unittest.TestCase):
@@ -31,11 +32,22 @@ class MultipleStationsNoOpposite(unittest.TestCase):
     def setUpClass(cls):
         cls.M = 50
         cls.tracks = [("s0", "s1"), ("s1", "s0"), ("s0", "s2"), ("s2", "s3")]
-        cls.agv_routes = {0: ("s0", "s1"), 1: ("s0", "s2", "s3")}
+        cls.agv_routes = {0: ("s0", "s1"), 1: ("s0", "s2", "s3"), 2: ("s2", "s3")}
         cls.graph = utils.create_graph(cls.tracks, cls.agv_routes)
         cls.iterators = utils.create_iterators(cls.graph, cls.agv_routes)
         cls.initial_conditions = {("in", 0, "s0"): 1, ("in", 1, "s0"): 3}
-        cls.d_max = {j: 10 for j in cls.agv_routes.keys()}
+        cls.d_max = {j: 100 for j in cls.agv_routes.keys()}
+        cls.weights = {j: 1 for j in cls.agv_routes.keys()}
+
+        all_same_way = utils.create_same_way_dict(cls.agv_routes)
+        tracks_len = {("s0", "s1"): 1, ("s1", "s0"): 1, ("s0", "s2"): 2, ("s2", "s3"): 2}
+        J = utils.create_agv_list(cls.agv_routes)
+        stations = utils.create_stations_list(cls.tracks)
+        agv_routes_as_edges = utils.agv_routes_as_edges(cls.agv_routes)
+
+        cls.tau_headway = {(j, jp, s, sp): 2 for (j, jp) in all_same_way.keys() for (s, sp) in all_same_way[(j, jp)]}
+        cls.tau_pass = {(j, s, sp): tracks_len[(s, sp)] for j in J for s, sp in agv_routes_as_edges[j]}
+        cls.tau_operation = {(agv, station): 2 for agv in J for station in stations}
 
     def test_preference_variables_y_multi(self):
         PVY, PVY_b = linear_solver.create_precedence_matrix_y(self.iterators)
@@ -43,20 +55,68 @@ class MultipleStationsNoOpposite(unittest.TestCase):
         #np.testing.assert_array_equal(PVY, np.array([[0, 0, 0, 0, 1, 1]]))
         #self.assertEqual(PVY_b, np.array([1]))
 
-    def test_create_bounds_multi(self):
-        bounds = linear_solver.create_bounds(self.initial_conditions, self.d_max, self.iterators)
-        self.assertEqual(bounds, [(1, 11), (0, 10), (3, 13), (0, 10), (0, 10), (0, 10), (0, 10),
-                                  (0, 10), (0, 10), (0, 10), (0, 1), (0, 1)])
+    def test_tau_headway(self):
 
+        self.assertEqual(self.tau_headway, {(1, 2, "s2", "s3"): 2, (2, 1, "s2", "s3"): 2})
+
+    def test_create_bounds_multi(self):
+        bounds = linear_solver.create_bounds(self.initial_conditions, self.d_max, self.iterators) # TODO
+        # self.assertEqual(bounds, [(1, self.d_max[0] + 1), (0, self.d_max[0]), (3, self.d_max[0] + 3),
+        #                          (0, self.d_max[0]), (0, self.d_max[0]), (0, self.d_max[0]), (0, self.d_max[0]),
+        #                          (0, self.d_max[0]), (0, self.d_max[0]), (0, self.d_max[0]), (0, 1), (0, 1)])
+
+    def test_headway_matrix(self):
+        x_iter = self.iterators["x"]
+        MH, MH_b = linear_solver.create_minimal_headway_matrix(self.M, self.tracks, self.agv_routes,
+                                                               self.tau_headway, self.iterators)
+        self.assertEqual(MH.shape, (2, 20))
+
+    def test_solve(self):
+        res, x_iter = linear_solver.solve(self.M, self.tracks, self.agv_routes, self.d_max,
+                                          self.tau_pass, self.tau_headway, self.tau_operation,
+                                          self.weights, self.initial_conditions)
+        #self.assertTrue(res.success)
 
 
 class TwoStationsOpposite(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+
         cls.M = 50
         cls.tracks = [("s0", "s1"), ("s1", "s2"), ("s2", "s1")]
-        cls.agv_routes = {0: ("s0", "s1", "s2"), 1: ("s1", "s0"), 2: ("s2", "s1")}
+        cls.agv_routes = {0: ("s0", "s1"), 1: ("s0", "s1", "s2"), 2: ("s2", "s1")}
+        cls.graph = utils.create_graph(cls.tracks, cls.agv_routes)
+        cls.iterators = utils.create_iterators(cls.graph, cls.agv_routes)
+        cls.d_max = {j: 10 for j in cls.agv_routes.keys()}
+        cls.weights = {j:1 for j in cls.agv_routes.keys()}
 
-#if __name__ == '__main__':
-#    unittest.main()
+        all_same_way = utils.create_same_way_dict(cls.agv_routes)
+        tracks_len = {("s0", "s1"): 1, ("s1", "s2"): 2, ("s2", "s1"): 2}
+        J = utils.create_agv_list(cls.agv_routes)
+        stations = utils.create_stations_list(cls.tracks)
+        agv_routes_as_edges = utils.agv_routes_as_edges(cls.agv_routes)
+
+        cls.tau_headway = {(j, jp, s, sp): 2 for (j, jp) in all_same_way.keys() for (s, sp) in all_same_way[(j, jp)]}
+        cls.tau_pass = {(j, s, sp): tracks_len[(s, sp)] for j in J for s, sp in agv_routes_as_edges[j]}
+        cls.tau_operation = {(agv, station): 2 for agv in J for station in stations}
+
+    def test_tau_headway(self):
+
+        self.assertEqual(self.tau_headway, {(0, 1, "s0", "s1"): 2, (1, 0, "s0", "s1"): 2})
+
+    def test_headway_matrix(self):
+
+        MH, MH_b = linear_solver.create_minimal_headway_matrix(self.M, self.tracks, self.agv_routes,
+                                                               self.tau_headway, self.iterators)
+        self.assertEqual(MH.shape, (2, 24))
+
+    def test_solve(self):
+        res, x_iter = linear_solver.solve(self.M, self.tracks, self.agv_routes, self.d_max,
+                                          self.tau_pass, self.tau_headway, self.tau_operation,
+                                          self.weights, initial_conditions={})
+
+        #self.assertTrue(res.success)
+
+if __name__ == '__main__':
+    unittest.main()
