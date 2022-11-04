7b3e994ad6e8a0fb68d8408d0dc0358b3d109455
tomsmierz <smirzchalski.tomek@gmail.com>
2022-11-04 12:31:26 +0100

    commented qubo solver, added docplex solve

--- examples/example_medium.py
+++ examples/example_medium.py
@@ -1,11 +1,12 @@
 from src import utils
 from src.linear_solver import solve
-from src.linear_solver import make_linear_problem
+from src.linear_solver import make_linear_problem, create_linear_model
 from src import train_diagram
 from src.qubo_solver import annealing
 from src.linear_solver import print_ILP_size
 import numpy as np
 import pickle
+import time
 
 from src.LinearProg import LinearProg
 from src.process_results import print_results
@@ -75,38 +76,49 @@ else:
 
 print_ILP_size(A_ub, b_ub, A_eq, b_eq)
 
+model = create_linear_model(obj, A_ub, b_ub, A_eq, b_eq, bounds, iterators)
+model.print_information()
+begin = time.time()
+s = model.solve()
+end = time.time()
+print("time: ", end-begin)
+model.print_solution(print_zeros=True)
+print(model.solve_details)
+
+
+
 # QUBO
-
-lp = LinearProg(c=obj, bounds=bounds, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq)
-p = 2.75
-
-with open("lp_medium.pkl", "wb") as f:
-    pickle.dump(lp, f)
-
-lp._to_bqm_qubo_ising(p)
-lp._to_cqm()
-
-
-
-print("-----------------------------------------------------")
-print("Number of q-bits", lp._count_qubits())
-print("Number of couplings Js:", lp._count_quadratic_couplings())
-print("Number of local filds hs:", lp._count_linear_fields())
-print("-----------------------------------------------------")
-
-simulation = False
-
-if simulation:
-    sdict={"num_sweeps":10_000, "num_reads":1_000, "beta_range":(0.0001, 100)}
-    dict_list = annealing(lp, "sim", "7_AGV", sim_anneal_var_dict=sdict, load=False, store=False)
-    print("Simulated annealing results")
-    print_results(dict_list)
-
-dict_list = annealing(lp, "hyb", "7_AGV", load=False, store=True)
-print("QPU results")
-print_results(dict_list)
-
-
+#
+# lp = LinearProg(c=obj, bounds=bounds, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq)
+# p = 2.75
+#
+# with open("lp_medium.pkl", "wb") as f:
+#     pickle.dump(lp, f)
+#
+# lp._to_bqm_qubo_ising(p)
+# lp._to_cqm()
+#
+#
+#
+# print("-----------------------------------------------------")
+# print("Number of q-bits", lp._count_qubits())
+# print("Number of couplings Js:", lp._count_quadratic_couplings())
+# print("Number of local filds hs:", lp._count_linear_fields())
+# print("-----------------------------------------------------")
+#
+# simulation = False
+#
+# if simulation:
+#     sdict={"num_sweeps":10_000, "num_reads":1_000, "beta_range":(0.0001, 100)}
+#     dict_list = annealing(lp, "sim", "7_AGV", sim_anneal_var_dict=sdict, load=False, store=False)
+#     print("Simulated annealing results")
+#     print_results(dict_list)
+#
+# dict_list = annealing(lp, "hyb", "7_AGV", load=False, store=True)
+# print("QPU results")
+# print_results(dict_list)
+#
+#
 
 """
 
