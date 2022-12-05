526f267e64920650ff92083822ee8ea275fb6b18
tomsmierz <smirzchalski.tomek@gmail.com>
2022-12-05 13:18:37 +0100

    minor changes

--- src/qubo_to_matrix.py
+++ src/qubo_to_matrix.py
@@ -3,10 +3,11 @@ from src.utils import qubo_to_matrix
 import scipy.sparse as sparse
 
 if __name__ == "__main__":
+    size = "large"
     p = 2.75
-    lp = load_linear_prog_object("lp_files/lp_largest.pkl")
+    lp = load_linear_prog_object(f"lp_files/lp_{size}.pkl")
     lp._to_bqm_qubo_ising(p)
     qubo = lp.qubo[0]
     matrix = qubo_to_matrix(qubo, lp)
     matrix = sparse.coo_matrix(matrix)
-    sparse.save_npz("largest_qubo_coo.npz", matrix)
+    sparse.save_npz(f"qubo/{size}_qubo_coo.npz", matrix)
