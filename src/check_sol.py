895261da2939a493184ff6f5e7353f716f2e7f7a
tomsmierz <smirzchalski.tomek@gmail.com>
2023-08-10 17:42:33 +0200

    rewritten

--- src/check_sol.py
+++ src/check_sol.py
@@ -1,43 +1,40 @@
 import pickle
-
-from dimod.sampleset import SampleSet
-from src.process_results import get_results
+import os
+import dimod
+import pyqubo
+from src.process_results import get_results, analyze_constraints, get_objective
 from src.quadratic_solver_CPLEX import load_linear_prog_object, process_result
 import json
 from src.utils import check_solution_list
 import pandas as pd
-import os
+
+from pathlib import Path
 
 cwd = os.getcwd()
+sol_folder = Path("annealing_results/7_AGV")
+lp_folder = Path("lp_files")
 
-# with open("results/out_large.json", "r") as f:
-#     res = json.load(f)
-#
-# lp = load_linear_prog_object("lp_files/lp_large.pkl")
-#
-#
-# res = res["result"]
-# print(check_solution_list(res, lp))
+hybrid = "cqm"
 
-df = pd.read_csv(os.path.join(cwd, "..", "results", "results_sb.csv"), sep=";")
+with open(os.path.join(cwd, "..", sol_folder, f"new_{hybrid}.pkl"), "rb") as f:
+    sampleset = pickle.load(f)
 
+with open(os.path.join(cwd, "..", lp_folder, "lp_medium.pkl"), "rb") as f:
+    lp = pickle.load(f)
 
-def create_results_dict(df):
-    results_dict = {}
-    for row in df.itertuples():
-        results_dict[row.instance[0:-4]] = list(eval(row.ising_state))
-    return results_dict
+sampleset = dimod.SampleSet.from_serializable(sampleset)
 
+if __name__ == '__main__':
 
-def read_lp(name):
-    with open(os.path.join(cwd, "..", f"lp_{name}.pkl"), "rb") as f:
-        lp = pickle.load(f)
-        return lp
+    if hybrid == "bqm":
+        print(sampleset.info)
+        p=5
+        lp._to_bqm_qubo_ising(p)
+        sampleset = lp.interpreter(sampleset)
+        for sol in get_results(sampleset, lp):
+            print(sol)
 
-
-if __name__ == "__main__":
-    name = "large"
-    results_dict = create_results_dict(df)
-    lp = load_linear_prog_object(os.path.join(cwd, "..", "lp_files", f"lp_{name}.pkl"))
-    spins = results_dict[name]
-    print(check_solution_list(spins, lp))
+    elif hybrid == "cqm":
+        print(sampleset.info)
+        for sol in get_results(sampleset, lp):
+            print(sol)
