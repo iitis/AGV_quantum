a6fe0fe573f322175fd0ff1ee0449f27a9404e11
Krzysztof Domino <kdomino@iitis.pl>
2023-10-04 08:52:25 +0200

    rename modules

--- AGV_quantum/__init__.py
+++ AGV_quantum/__init__.py
@@ -1,33 +1,33 @@
 
-from AGV_quantum.LinearProg import LinearProg
+from .LinearProg import LinearProg
 
 
-from AGV_quantum.process_results import (
+from .process_results import (
     get_results, load_results, store_result, print_results
 )
 
-from AGV_quantum.quadratic_solver_CPLEX import (
+from .quadratic_solver_CPLEX import (
     load_linear_prog_object, process_result, quadratic_solve_qubo
 )
 
 
-from AGV_quantum.utils import(
+from .utils import(
     see_non_zero_variables, create_graph, create_iterators, create_agv_list, check_solution_list, qubo_to_matrix,
     create_stations_list, create_agv_list, agv_routes_as_edges, create_same_way_dict, create_v_in_out,
     create_t_iterator, create_y_iterator, create_z_iterator
 )
 
 
-from AGV_quantum.linear_solver import (
+from .linear_solver import (
      LinearAGV, print_ILP_size
 )
 
-from AGV_quantum.quadratic_solver import QuadraticAGV
+from .quadratic_solver import QuadraticAGV
 
-from AGV_quantum.qubo_solver import (
+from .qubo_solver import (
     sim_anneal, annealing, constrained_solver, hybrid_anneal
 )
 
 
-from AGV_quantum.train_diagram import plot_train_diagram
+from .train_diagram import plot_train_diagram
 
