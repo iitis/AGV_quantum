9281457d3970f4f4b8813c011e61eb25563219e5
Krzysztof Domino <kdomino@iitis.pl>
2024-07-01 13:49:27 +0200

    finalyze data processing

--- make_statistics.py
+++ make_statistics.py
@@ -1,6 +1,8 @@
 import pickle
 import numpy as np
 import os
+import csv
+
 from pathlib import Path
 import dimod
 from AGV_quantum import get_results
@@ -79,6 +81,9 @@ def read_smapleset(size, k, solver):
 
 def print_info(key, sizes):
     print(key)
+    no_qbits = []
+    means = []
+    stds = []
     for size in sizes:
         vars = get_no_vars(size)
         series = read_series(size, "cqm", key)
@@ -88,8 +93,81 @@ def print_info(key, sizes):
             k=1000000
         else:
             k = 1
+        m = np.mean(series/k)
+        s = np.std(series/k)
+
+        no_qbits.append(vars)
+        means.append(m)
+        stds.append(s)
         
-        print(vars, np.mean(series/k), np.std(series/k))
+
+        print(vars, m, m-s, m+s)
+
+    return no_qbits, means, stds
+
+
+def print_obj(sizes, examples, optimum):
+
+    no_qbits = []
+    means = []
+    stds = []
+
+    print("objective")
+    for size in sizes:
+        obj = obj_cqm_array(examples, size)
+        obj = obj/optimum[size]
+        vars = get_no_vars(size)
+
+        m = np.mean(obj)
+        s = np.std(obj)
+        print(vars, m,m-s, m+s)
+
+
+        no_qbits.append(vars)
+        means.append(m)
+        stds.append(s)
+        
+        print(obj)
+        print(np.mean(obj), np.std(obj))
+
+    return no_qbits, means, stds
+
+
+def print_feas(sizes, examples):
+
+    no_qbits = []
+    means = []
+    stds = []
+
+    print("feasibility perc")
+    for size in sizes:
+        vars = get_no_vars(size)
+        perc = feas_cqm_array(examples, size)
+
+        m = np.mean(perc)
+        s = np.std(perc)
+        print(vars, m,m-s, m+s)
+
+
+        no_qbits.append(vars)
+        means.append(m)
+        stds.append(s)
+
+    
+    return no_qbits, means, stds
+
+
+
+
+def csv_write(file_name, no_qbits, means, stds):
+    with open(file_name, 'w', newline='', encoding="utf-8") as csvfile:
+        fieldnames = ["no_qbits", "mean", "mean-std", "mean+std"]
+
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        for i,v in enumerate(no_qbits):
+            m = means[i]
+            s = stds[i]
+            writer.writerow({"no_qbits": v, "mean": np.round(m, 4), "mean-std": np.round(m-s, 4), "mean+std": np.round(m+s, 4)})
 
 
 
@@ -110,23 +188,33 @@ if __name__ == "__main__":
 
 
     key = 'qpu_access_time'
-    print_info(key, sizes)
+    no_qbits, means, stds = print_info(key, sizes)
+
+    file = "article_plots/CQM_QPU_time.csv"
+    csv_write(file, no_qbits, means, stds)
 
     key = 'run_time'
-    print_info(key, sizes)
+    no_qbits, means, stds = print_info(key, sizes)
 
-    print("feasibility perc")
-    for size in sizes:
-        perc = feas_cqm_array(examples, size)
-        print(np.mean(perc), np.std(perc))
+    file = "article_plots/time_CQM.csv"
+    csv_write(file, no_qbits, means, stds)
 
-    print("objective")
-    for size in sizes:
-        obj = obj_cqm_array(examples, size)
-        obj = obj/optimum[size]
-        
-        print(obj)
-        print(np.mean(obj), np.std(obj))
+    no_qbits, means, stds = print_feas(sizes, examples)
+
+    file = "article_plots/feasibility_CQM.csv"
+    csv_write(file, no_qbits, means, stds)
+
+    # TODO we do not have yet CPLEX largest ever
+    examples = {2:"smallest", 4:"small", 6:"medium_small", 7:"medium", 12:"large", 15:"largest"}
+    sizes = examples.keys()
+
+    no_qbits, means, stds = print_obj(sizes, examples, optimum)
+
+    file = "article_plots/obj_CQM.csv"
+    csv_write(file, no_qbits, means, stds)
+
+
+
 
 
 
